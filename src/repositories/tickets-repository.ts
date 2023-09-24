fe2308ca79ea3d165406124e563758f19e18a58b
hp1heloisa <heloisapimentel2202@gmail.com>
2023-09-24 01:24:07 -0300

    teste

--- src/repositories/tickets-repository.ts
+++ src/repositories/tickets-repository.ts
@@ -12,10 +12,10 @@ async function getUserTickets(userId: number){
             SELECT "Ticket".id, "Ticket".status, "Ticket"."ticketTypeId", "Ticket"."enrollmentId", 
             json_build_object('id',"TicketType".id, 'name', "TicketType"."name", 'price', "TicketType".price, 
             'isRemote', "TicketType"."isRemote", 'includesHotel', "TicketType"."includesHotel", 'createdAt', 
-            to_char("Ticket"."createdAt", 'YYYY-MM-DD"T"HH24:MI:SS.MS3"Z"'), 'updatedAt', 
-            to_char("Ticket"."updatedAt", 'YYYY-MM-DD"T"HH24:MI:SS.MS3"Z"')) as "TicketType", 
-            to_char("Ticket"."createdAt", 'YYYY-MM-DD"T"HH24:MI:SS.MS3"Z"') as "createdAt", 
-            to_char("Ticket"."updatedAt", 'YYYY-MM-DD"T"HH24:MI:SS.MS3"Z"') as "updatedAt" FROM "Enrollment" JOIN "User" 
+            to_char("TicketType"."createdAt", 'YYYY-MM-DD"T"HH24:MI:SS.MS"Z"'), 'updatedAt', 
+            to_char("TicketType"."updatedAt", 'YYYY-MM-DD"T"HH24:MI:SS.MS"Z"')) as "TicketType", 
+            to_char("Ticket"."createdAt", 'YYYY-MM-DD"T"HH24:MI:SS.MS"Z"') as "createdAt", 
+            to_char("Ticket"."updatedAt", 'YYYY-MM-DD"T"HH24:MI:SS.MS"Z"') as "updatedAt" FROM "Enrollment" JOIN "User" 
             ON "Enrollment"."userId" = "User".id  JOIN "Ticket" ON 
             "Ticket"."enrollmentId"="Enrollment".id JOIN "TicketType" ON "Ticket"."ticketTypeId"="TicketType".id WHERE
             "User".id=${userId};
@@ -28,8 +28,7 @@ async function postTicket(ticketTypeId: number, enrollmentId: number){
         data:{
             ticketTypeId,
             enrollmentId,
-            status: 'RESERVED',
-            updatedAt: dayjs().format()
+            status: 'RESERVED'
         }
     })
 }
